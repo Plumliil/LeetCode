3ba867884e85d53164aa6daac72bf0c900dfbeb0
plumli <476453574@qq.com>
2022-03-09 21:37:59 +0800

    21_合并两个有序链表

---
+++ code/链表/21_合并两个有序链表.js
@@ -0,0 +1,19 @@
+// 21. 合并两个有序链表
+// 将两个升序链表合并为一个新的 升序 链表并返回。
+// 新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+
+
+var mergeTwoLists = function (list1, list2) {
+
+};
+
+
+const { createList } = require('../util/createList')
+
+l1 = [1, 2, 4], l2 = [1, 3, 4]
+l1 = createList(l1)
+l2 = createList(l2)
+// mergeTwoLists(l1,l2)
+console.log(mergeTwoLists(l1, l2));
+// console.log(l1,l2);
