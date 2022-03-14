de28a05a08dc1de0a23d4650d36cf8859b4ad798
plumli <476453574@qq.com>
2022-03-14 22:07:10 +0800

    水果成篮

--- code/index.js
+++ code/index.js
@@ -98,34 +98,36 @@
 // 你想要尽可能多地收集水果。然而，农场的主人设定了一些严格的规矩，你必须按照要求采摘水果：
 
 // 你只有 两个 篮子，并且每个篮子只能装 单一类型 的水果。每个篮子能够装的水果总量没有限制。
-// 你可以选择任意一棵树开始采摘，你必须从 每棵 树（包括开始采摘的树）上 恰好摘一个水果 。采摘的水果应当符合篮子中的水果类型。每采摘一次，你将会向右移动到下一棵树，并继续采摘。
+// 你可以选择任意一棵树开始采摘，你必须从 每棵 树（包括开始采摘的树）上 恰好摘一个水果 。
+//  采摘的水果应当符合篮子中的水果类型。每采摘一次，你将会向右移动到下一棵树，并继续采摘。
 // 一旦你走到某棵树前，但水果不符合篮子的水果类型，那么就必须停止采摘。
 // 给你一个整数数组 fruits ，返回你可以收集的水果的 最大 数目。
 
 
-// /**
-//  * @param {number[]} fruits
-//  * @return {number}
-//  */
-// var totalFruit = function (fruits) {
-//     const len = fruits.length;
-//     let f = s =  0;
-//     let ans=len+1;
-//     let basket = new Set();
-//     // let basket=[];
-//     while (f < len) {
-//         if (basket.size <= 2 || !basket.has(fruits[f])) {
-//             basket.add(fruits[f++]);
-//         } else {
-//             basket.delete(fruits[s++]);
-//         }
-//         // if(!basket)
-//     }
-//     console.log(ans);
-// };
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+    let start = end = 0;
+    let set=new Set();
+    let ans=0;
+    let len=fruits.length;
+    while(start<len){
+        set.add(fruits[end]);
+        if(set.size<=2){
+            ans=Math.max(ans,end-start+1);
+        }else{
+            let count=1;
+            while(fruits[end-1]===fruits[end-1-count]){
+                count++;
+            }
+        }
+    }
+};
 
 
-// // fruits = [1, 2, 3, 2, 2];
-// // fruits = [0,1,2,2]
-// fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]
-// totalFruit(fruits)
+// fruits = [1, 2, 3, 2, 2];
+// fruits = [0,1,2,2]
+fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]
+totalFruit(fruits)
