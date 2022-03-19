00a27209af27ba0e08a501b50787a61ec7483229
plumli <476453574@qq.com>
2022-03-19 21:08:30 +0800

    34_在排序列表中找元素第一个最后一个位置

--- code/index.js
+++ code/index.js
@@ -1,6 +1,4 @@
 
-// // console.log(newHead);
-
 // 904. 水果成篮
 
 // 你正在探访一家农场，农场从左到右种植了一排果树。这些树用一个整数数组 fruits 表示，其中 fruits[i] 是第 i 棵树上的水果 种类 。
@@ -14,30 +12,76 @@
 // 给你一个整数数组 fruits ，返回你可以收集的水果的 最大 数目。
 
 
+// /**
+//  * @param {number[]} fruits
+//  * @return {number}
+//  */
+// var totalFruit = function (fruits) {
+//     let start = end = 0;
+//     let set=new Set();
+//     let ans=0;
+//     let len=fruits.length;
+//     while(start<len){
+//         set.add(fruits[end]);
+//         if(set.size<=2){
+//             ans=Math.max(ans,end-start+1);
+//         }else{
+//             let count=1;
+//             while(fruits[end-1]===fruits[end-1-count]){
+//                 count++;
+//             }
+//         }
+//     }
+// };
+
+
+// // fruits = [1, 2, 3, 2, 2];
+// // fruits = [0,1,2,2]
+// fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]
+// totalFruit(fruits)
+
+
+// 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+
+// 进阶：
+
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+
+
 /**
- * @param {number[]} fruits
- * @return {number}
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
-var totalFruit = function (fruits) {
-    let start = end = 0;
-    let set=new Set();
-    let ans=0;
-    let len=fruits.length;
-    while(start<len){
-        set.add(fruits[end]);
-        if(set.size<=2){
-            ans=Math.max(ans,end-start+1);
-        }else{
-            let count=1;
-            while(fruits[end-1]===fruits[end-1-count]){
-                count++;
+var searchRange = function (nums, target) {
+    if (nums.length === 0 || nums[0] > target || nums[nums.length - 1] < target) return [-1, -1];
+    let f = s = ans = 0;
+    let len = nums.length;
+    while (len-- > 0) {
+        if (nums[f] === target) {
+            s = f;
+            for (; s < nums.length; s++) {
+                if (nums[s] === target) {
+                    ans = s;
+                }
             }
+            break;
+        } else {
+            f++;
         }
     }
+    return f > s ? [-1, -1] : [f, ans];
 };
 
+nums = [5, 7, 7, 8, 8, 8, 10], target = 8;
+// nums = [5,7,7,8,8,10], target = 6;
+// nums = [], target = 0;
+// nums = [1], target = 0;
+// nums=[2,2],target=2
+// nums = [1, 4], target = 4
 
-// fruits = [1, 2, 3, 2, 2];
-// fruits = [0,1,2,2]
-fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]
-totalFruit(fruits)
+
+
+console.log(searchRange(nums, target));
---
+++ code/指针/34_在排序列表中找元素第一个最后一个位置.js
@@ -0,0 +1,44 @@
+// 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+
+// 进阶：
+
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var searchRange = function (nums, target) {
+    if (nums.length === 0 || nums[0] > target || nums[nums.length - 1] < target) return [-1, -1];
+    let f = s = ans = 0;
+    let len = nums.length;
+    while (len-- > 0) {
+        if (nums[f] === target) {
+            s = f;
+            for (; s < nums.length; s++) {
+                if (nums[s] === target) {
+                    ans = s;
+                }
+            }
+            break;
+        } else {
+            f++;
+        }
+    }
+    return f > s ? [-1, -1] : [f, ans];
+};
+
+nums = [5, 7, 7, 8, 8, 8, 10], target = 8;
+// nums = [5,7,7,8,8,10], target = 6;
+// nums = [], target = 0;
+// nums = [1], target = 0;
+// nums=[2,2],target=2
+// nums = [1, 4], target = 4
+
+
+
+console.log(searchRange(nums, target));
